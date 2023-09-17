fbd7dd11acc16d44b1037fa7638cbee15cf9f4ae
Pavel Mokhov <mo.pm@icloud.com>
2023-09-17 16:51:23 +0700

    TWI-49-done-right fix: bugs

--- internal/tweets/create.go
+++ internal/tweets/create.go
@@ -1,7 +1,9 @@
 package tweets
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"time"
 
@@ -27,23 +29,36 @@ func (s *Service) Create(w http.ResponseWriter, r *http.Request) {
 		services.ReturnErr(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	if err := req.validate(); err != nil {
-		services.ReturnErr(w, err, http.StatusBadRequest)
-		return
-	}
-
-	query := `INSERT INTO tweets (user_id, text, created_at, public, only_followers, only_mutual_followers, only_me)
-		VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING tweet_id`
-	err = s.DB.QueryRowContext(r.Context(), query, userID, req.Text, time.Now(), req.Public, req.OnlyFollowers, req.OnlyMutualFollowers, req.OnlyMe).Scan(&req.ID)
-	if err != nil {
+	if err = s.CreateNewTweet(req, r.Context(), userID); err != nil {
 		services.ReturnErr(w, err.Error(), http.StatusInternalServerError)
 		return
-
 	}
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(req)
 
 	return
+}
+func (s *Service) CreateNewTweet(req createTweetRequest, ctx context.Context, parentID int) error {
+	if err := req.validate(); err != nil {
+		return err
+	}
+	userID := ctx.Value("userID").(int)
+	if !req.isValid() {
+		return errors.New("visibility error, many visual arguments")
+	}
+	if req.defaultVisibilities() {
+		req.Visibility.Public = true
+		req.Visibility.OnlyMe = false
+		req.Visibility.OnlyFollowers = false
+		req.Visibility.OnlyMutualFollowers = false
+	}
+	query := `INSERT INTO tweets (user_id, text, created_at,parent_tweet_id, public, only_followers, only_mutual_followers, only_me)
+		VALUES ($1, $2, $3, $4, $5, $6, $7,$8)`
+	if err := s.DB.QueryRowContext(ctx, query, userID, req.Text, time.Now(), parentID, req.Public, req.OnlyFollowers, req.OnlyMutualFollowers, req.OnlyMe).Err(); err != nil {
+		return err
+	}
+	return nil
+
 }
 
 func (s createTweetRequest) validateText(fl validator.FieldLevel) bool {
